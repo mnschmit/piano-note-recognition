3da569fdd9c09f5b9f002079cc059ebf2f3d308d
mnschmit <m.schmitt7@gmx.de>
2016-01-13 14:11:28 +0100

    added a simple visualization of midi matrix to simple.py (to be continued ...)

--- simple.py
+++ simple.py
@@ -3,10 +3,13 @@
 from sys import argv
 
 filename = argv[1]
-
-n_components=2
+midi_filename = "/home/martin/uni/music/miniproject/reference/ty_januarMINp_align.mid"
 if len(argv) > 2:
-    n_components = int(argv[2])
+    midi_filename = argv[2]
+
+n_components=None
+if len(argv) > 3:
+    n_components = int(argv[3])
 
 from librosa import load, cqt, logamplitude
 import numpy as np
@@ -15,11 +18,19 @@ import numpy as np
 x, sr = load(filename)
 
 # compute constant-Q transform (~ pitch-based STFT)
-C = cqt(x, sr=sr)
+hop_size = 512
+C = cqt(x, sr=sr, hop_length=hop_size)
+#C = stft(x, hop_length=hop_size)
+
+# try some midi visualization
+from Midi import midi_matrix
+
+midi_mat = midi_matrix(midi_filename)
 
 # NMF
 
-V = np.log10(1 + 100000 * C**2)
+#V = np.log10(1 + 100000 * C**2)
+V = np.abs(C)
 
 from librosa.decompose import decompose
 
@@ -28,25 +39,32 @@ comps, acts = decompose(V, n_components=n_components, sort=True)
 # visualisation matters
 import matplotlib.pyplot as plt
 from librosa.display import specshow
+import matplotlib.gridspec as gridspec
 
-plt.subplot(3, 1, 1)
+plt.close('all')
+
+plt.subplot2grid((4, 2), (0,0), colspan=2)
+specshow(midi_mat, sr=sr, x_axis='time', y_axis='cqt_note')
+plt.title('midi visualization')
+
+plt.subplot2grid((4, 2), (1,0), colspan=2)
 specshow(V, sr=sr, x_axis='time', y_axis='cqt_note')
 #plt.ylabel('pitches')
 plt.colorbar(format='%+2.0f dB')
 plt.title('Input Constant-Q power spectrum')
 
-plt.subplot(3, 2, 3)
+plt.subplot2grid((4, 2), (2,0))
 specshow(comps, y_axis='cqt_note')
 #plt.ylabel('pitches')
 plt.xlabel('Index')
 plt.title('Learned Components')
 
-plt.subplot(3, 2, 4)
+plt.subplot2grid((4, 2), (2,1))
 specshow(acts, x_axis='time')
 plt.ylabel('Components')
 plt.title('Activations')
 
-plt.subplot(3, 1, 3)
+plt.subplot2grid((4, 2), (3,0), colspan=2)
 V_approx = comps.dot(acts)
 specshow(V_approx, sr=sr, x_axis='time', y_axis='cqt_note')
 #plt.ylabel('pitches')
